896334df197dd35be29364d48989af23338074ca
jiwonc <alice010503@gmail.com>
2021-01-29 11:06:09 +0900

    생성 후 첫 푸시

---
+++ demo/src/main/java/com/example/demo/controller/BoardController.java
@@ -0,0 +1,31 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.BoardDto;
+import com.example.demo.service.BoardService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+    private BoardService boardService;
+
+    public BoardController (BoardService boardService){
+        this.boardService = boardService;
+    }
+    @GetMapping("/")
+    public String list() {
+        return "board/list.html";
+    }
+
+    @GetMapping("/post")
+    public String post() {
+        return "board/post.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto) {
+        boardService.savePost(boardDto);
+        return "redirect:/";
+    }
+}
